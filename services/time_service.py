b7d0016a10e20aa306ad6669b0a2908631a5e147
Vladyslav Futrak <vladyslav.futrak@student.tuke.sk>
2026-08-08 21:35:22 +0200

    Refactor time synchronization logic to use socket communication for improved reliability

--- services/time_service.py
+++ services/time_service.py
@@ -1,4 +1,7 @@
 import ntptime
+import machine
+import socket
+import struct
 import time
 
 from config import NTP_SERVER, TIMEZONE_OFFSET_SECONDS
@@ -6,12 +9,29 @@ from config import NTP_SERVER, TIMEZONE_OFFSET_SECONDS
 
 class TimeService:
 
-    def sync(self):
+    def sync(self, timeout_seconds=10):
         try:
             print(f"Synchronizing internet time from {NTP_SERVER}...")
 
-            ntptime.host = NTP_SERVER
-            ntptime.settime()
+            addr = socket.getaddrinfo(NTP_SERVER, 123)[0][-1]
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.settimeout(timeout_seconds)
+
+            try:
+                packet = bytearray(48)
+                packet[0] = 0x1B
+                s.sendto(packet, addr)
+                data, _ = s.recvfrom(48)
+            finally:
+                s.close()
+
+            if len(data) < 48:
+                raise ValueError("Invalid NTP response")
+
+            seconds = struct.unpack(">I", data[40:44])[0] - 2208988800
+            tm = time.gmtime(seconds)
+            rtc = machine.RTC()
+            rtc.datetime((tm[0], tm[1], tm[2], tm[6], tm[3], tm[4], tm[5], 0))
 
             print("Time synchronized")
             return True
